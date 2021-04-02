ac7d94a54b0cad3956c65eb7912f874927d97598
Loycine <luoyucheng99@gmail.com>
2021-04-02 21:52:44 +0800

    add lab1

---
+++ lab1/README.md
@@ -0,0 +1,24 @@
+# Compile & Run
+```bash
+
+gcc main.c -o main
+./main
+
+```
+
+# Input 
+```
+First choose a command from 1-4
+1 Create List
+2 Printf List
+3 Insert List
+4 Quit
+
+If you choose 3:
+You should then input a name(str) and a age(int) separated by a blank.
+For example:
+luoyucheng 22
+```
+
+# Demo Screenshot
+![avatar](./imgs/1.png)
---
+++ lab1/main.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//预定义数据结构
+typedef struct stuInfo {
+    char stuName[20]; /*学生姓名*/
+    int Age; /*年龄*/
+} ElemType;
+
+typedef struct node {
+    ElemType data;
+    struct node *next;
+} ListNode, *ListPtr;
+
+
+ListPtr ListHead = NULL;
+ListPtr CreateList()
+{
+    ListPtr newHead = malloc(sizeof(ListNode));
+    newHead->next = NULL;
+    return newHead;
+}
+
+void PrintList(ListPtr head)
+{
+    if(head==NULL)
+    {
+        return;
+    }
+    ListPtr cur = head->next;
+    while(cur != NULL)
+    {
+        printf("(%s,%d) ", cur->data.stuName, cur->data.Age);
+        cur = cur->next;
+    }
+    printf("\n");
+    return;
+}
+
+ListPtr InsertList(ListPtr pos)
+{
+    ListPtr before = pos;
+    ListPtr after = before->next;
+    ListPtr curNode = malloc(sizeof(ListNode));
+
+    scanf("%s%d", curNode->data.stuName, &curNode->data.Age);
+    before->next = curNode;
+    curNode->next = after;
+    return curNode;
+}
+
+int main(int argc, char* argv[])
+{
+    ListPtr currentNode = NULL;
+
+    printf("1 Create List\n");
+    printf("2 Printf List\n");
+    printf("3 Insert List\n");
+    printf("4 Quit\n");
+    while(1)
+    {
+        char command = getchar();
+        switch (command)
+        {
+        case '1': 
+            ListHead = CreateList();
+            currentNode = ListHead;
+            break;
+        case '2': PrintList(ListHead);
+            break;
+        case '3': currentNode = InsertList(currentNode);
+            break;
+        case '4':
+            return 0;
+        }
+    }
+    return 0;
+}
